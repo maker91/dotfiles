de9d80fc6f15c0848662d29aef441e06e85959e1
Matthew Reckless <mail@matthewreckless.co.uk>
2026-09-12 12:11:32 +0100

    More fixes

--- nvim/lua/scripts/clipboard.lua
+++ nvim/lua/scripts/clipboard.lua
@@ -1,12 +1,18 @@
-vim.g.clipboard = {
-  name = "win32yank-wsl",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf",
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf",
-  },
-  cache_enabled = true,
-}
+local is_wsl = (vim.fn.has("wsl") == 1) or (vim.env.WSLENV ~= nil) or (string.find(vim.uv.os_uname().release:lower(), "microsoft") ~= nil)
+
+if is_wsl then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = true,
+  }
+else
+  vim.g.clipboard = nil
+end
